9ba11a701e9cddb3cff93339eb24129f523a58ae
Adam M Jensiki <adam.jeniski@ibm.com>
2020-09-18 21:49:01 -0400

    add cors and make response more secure

--- api/server.js
+++ api/server.js
@@ -1,15 +1,25 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const cors = require('cors')
+
+const fs = require('fs');
+const path = require('path');
 
 const PORT = 3000;
 
 app.use(bodyParser.json());
+app.use(cors());
 
 app.post('/', (req, res) => {
-    return res.send({
-        response: eval('(function() { ' + req.body.code + ' }())')
-    })
+    fs.writeFileSync(path.join(__dirname, 'temp.js'), `module.exports = { userFunc: () => {\n\t${req.body.code}\n\t}\n}`);
+    const {userFunc} = require('./temp.js')
+    const response = userFunc();
+    console.log("User Code:", req.body.code, ", Response: ", response);
+    res.send({
+        response
+    });
+    delete require.cache[require.resolve('./temp.js')];
 });
 
 app.listen(PORT, () => {
